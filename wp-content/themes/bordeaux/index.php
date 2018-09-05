3be470d8851a70c10d6103ee9108d29c2ed7a499
Robrens <jbb.baudu@wanadoo.fr>
2018-09-05 15:56:20 +0200

    nav header

--- wp-content/themes/bordeaux/index.php
+++ wp-content/themes/bordeaux/index.php
@@ -17,7 +17,10 @@ get_header();
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main">
-
+			<div class="highlight">
+				<div class="presentation">
+				</div>
+			</div>
 		<?php
 		if ( have_posts() ) :
 
@@ -30,18 +33,23 @@ get_header();
 			endif;
 
 			/* Start the Loop */
+		
 			while ( have_posts() ) :
 				the_post();
 
 				/*
-				 * Include the Post-Type-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Type name) and that will be used instead.
-				 */
+				* Include the Post-Type-specific template for the content.
+				* If you want to override this in a child theme, then include a file
+				* called content-___.php (where ___ is the Post Type name) and that will be used instead.
+				*/
 				get_template_part( 'template-parts/content', get_post_type() );
-
+				
 			endwhile;
-
+			?>
+			
+		
+			
+			<?php 
 			the_posts_navigation();
 
 		else :
